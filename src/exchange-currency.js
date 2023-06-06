101f4ad41e775977d4ebffbc65983cdb2ae15589
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-06-06 03:16:28 -0600

    add fetch
    This fetch will be modify later on, we are only using it right now to guide us through the api

--- src/exchange-currency.js
+++ src/exchange-currency.js
@@ -17,3 +17,30 @@ function assignValue(selector, property, value) {
   const $element = (document.querySelector(selector)[property] = value);
   return $element;
 }
+
+fetch(URL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const $currencyList = document.querySelector("#currency-list");
+    console.log(data);
+    console.log(data.date);
+    console.log(data.rates);
+
+    let currencies = Object.keys(data.rates);
+    console.log(currencies);
+    currencies.forEach((currency, index) => {
+      const $li = document.createElement("li");
+      const $a = document.createElement("a");
+      $a.className = "dropdown-item";
+      $a.href = "#";
+      $a.innerText = `${currency} ${currenciesName[index]}`;
+
+      $li.appendChild($a);
+      $currencyList.appendChild($li);
+    });
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
