c6a0530a1906af833bf81001478b4ac1ec1972e2
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-31 23:15:38 -0600

    Creates initialize
    This function will serve us to run the program upon visitng the website.

--- src/exchange-currency.js
+++ src/exchange-currency.js
@@ -1,71 +1,39 @@
 const BASE_URL = "https://api.exchangerate.host";
 
-function convertCurrency(currency = "EUR", date = "latest") {
+function getExchangeRates(currency = "EUR", date = "latest") {
   const base = `${BASE_URL}/${date}?base=${currency}`;
 
-  fetch(base)
+  return fetch(base)
     .then((response) => {
       return response.json();
     })
-    .then((currencies) => {
-      createCurrencyTable(currencies);
+    .then((exchange) => {
+      return exchange.rates;
     })
     .catch((error) => {
       console.error("Error", error);
     });
 }
 
-function createCurrencyTable(currencies) {
-  const $currencyTable = document.querySelector("#currency-rates");
-  clearCurrenciesField("#currency-rates");
-
-  rates.forEach((rate, index) => {
-    const $tr = document.createElement("tr");
-    const $currencyNumber = document.createElement("th");
-    const $currency = document.createElement("th");
-    const $currencyFullName = document.createElement("th");
-    const $rate = document.createElement("th");
-
-    $currencyNumber.innerText = `${index + 1}`;
-    $currency.innerText = `${rate}`;
-    $currencyFullName.innerText = `${currenciesName[index]}`;
-    $rate.innerText = `${rates[rate]}`;
-    $tr.appendChild($currencyNumber);
-    $tr.appendChild($currency);
-    $tr.appendChild($currencyFullName);
-    $tr.appendChild($rate);
-    $currencyTable.appendChild($tr);
+function fetchExchangeRates(currencyCode = null, date = null) {
+  const exchanges = getExchangeRates(currencyCode, date);
+  exchanges.then((exchange) => {
+    createExchangeTable(exchange);
+    createCurrencyList(exchange);
   });
 }
 
-function createCurrencyList() {
-  fetch(`${BASE_URL}/latest`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const $currencyList = document.querySelector("#currency-list");
-      /*console.log(data);
-    console.log(data.date);
-    console.log(data.rates);*/
-
-      let currencies = Object.keys(data.rates);
-      console.log(currencies);
-      currencies.forEach((currency, index) => {
-        const $li = document.createElement("li");
-        const $a = document.createElement("a");
-        $a.className = "dropdown-item";
-        $a.href = "#";
-        $a.innerText = `${currency} ${currenciesName[index]}`;
-
-        $li.appendChild($a);
-        $currencyList.appendChild($li);
-      });
-    })
-    .catch((error) => {
-      console.log("Error", error);
-    });
+function handleCurrency() {
+  $currencyList.onclick = (event) => {
+    const $clickedCurrency = event.target;
+    const selectedCurrency = handleCurrencyList($clickedCurrency);
+    fetchExchangeRates(selectedCurrency);
+  };
 }
 
-convertCurrency();
-createCurrencyList();
+function initialize() {
+  fetchExchangeRates();
+  handleCurrency();
+}
+
+initialize();
