8c96a68d75afbf23c3cca094de15937ef3c94b75
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-31 23:13:00 -0600

    Update ui.js
    Adds the functions responsible for creating the table and list in the UI

--- src/ui.js
+++ src/ui.js
@@ -3,21 +3,42 @@ const $convertButton = document.querySelector("#convert-button");
 const $currency = document.querySelector("#currency-input").value;
 let $clickedCurrency;
 
-$currencyList.onclick = (event) => {
-  const $click = event.target;
-  $clickedCurrency = handleClicks($click);
+function createExchangeTable(rates) {
+  const $currencyTable = document.querySelector("#currency-rates");
+  const $rates = Object.keys(rates);
+  clearCurrenciesField("#currency-rates");
 
-  event.preventDefault();
-};
+  $rates.forEach((rate, index) => {
+    const $tr = document.createElement("tr");
+    const $currencyNumber = document.createElement("th");
+    const $currency = document.createElement("th");
+    const $currencyFullName = document.createElement("th");
+    const $rate = document.createElement("th");
 
-$convertButton.onclick = () => {
-  convertCurrency($currency);
-};
+    $currencyNumber.innerText = `${index + 1}`;
+    $currency.innerText = `${rate}`;
+    $currencyFullName.innerText = `${currenciesName[index]}`;
+    $rate.innerText = `${rates[rate]}`;
+    $tr.appendChild($currencyNumber);
+    $tr.appendChild($currency);
+    $tr.appendChild($currencyFullName);
+    $tr.appendChild($rate);
+    $currencyTable.appendChild($tr);
+  });
+}
 
-function handleClicks(click) {
-  if (click) {
-    const currencyName = obtainCurrencyCode(click);
-    const selectedCurrency = (document.querySelector("#currency-input").value = currencyName);
-    return selectedCurrency;
-  }
+function createCurrencyList(rates) {
+  const $currencyList = document.querySelector("#currency-list");
+  const currencies = Object.keys(rates);
+
+  currencies.forEach((currency, index) => {
+    const $li = document.createElement("li");
+    const $a = document.createElement("a");
+    $a.className = "dropdown-item";
+    $a.href = "#";
+    $a.innerText = `${currency} ${currenciesName[index]}`;
+
+    $li.appendChild($a);
+    $currencyList.appendChild($li);
+  });
 }
