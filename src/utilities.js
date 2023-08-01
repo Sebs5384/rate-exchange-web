873670addfe4c96991b4f0d807ecd7a0c7a3698c
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-07-31 23:10:57 -0600

    Update utilities

--- src/utilities.js
+++ src/utilities.js
@@ -6,3 +6,11 @@ function obtainCurrencyCode(currency) {
 function clearCurrenciesField(selector) {
   document.querySelector(selector).innerHTML = "";
 }
+
+function handleCurrencyList(click) {
+  if (click) {
+    const currencyName = obtainCurrencyCode(click);
+    const selectedCurrency = (document.querySelector("#currency-input").value = currencyName);
+    return selectedCurrency;
+  }
+}
