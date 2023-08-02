60bf9c87b551da10bfb21208ff2591f54285add8
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-08-01 20:52:29 -0600

    Updates exchange
    Moving all related UI functions to the ui module

--- src/exchange-currency.js
+++ src/exchange-currency.js
@@ -16,41 +16,9 @@ function getExchangeRates(currency = "EUR", date = "latest") {
     });
 }
 
-function displayExchangeUI(currencyCode = null, date = null) {
-  return getExchangeRates(currencyCode, date)
-    .then((rates) => {
-      clearCurrenciesField();
-      createExchangeTable(rates);
-      createCurrencyList(rates);
-    })
-    .catch((error) => {
-      console.error("Error while attempting to display exchange rates", error);
-      throw error;
-    });
-}
-
-function updateCurrency() {
-  const $selectedDate = document.querySelector("#currency-date").value;
-  const $currencyList = document.querySelector("#currency-list");
-  $currencyList.onclick = (event) => {
-    const $clickedCurrency = event.target;
-    const selectedCurrency = handleCurrencyListClick($clickedCurrency);
-    displayExchangeUI(selectedCurrency, selectedDate || null);
-  };
-
-  const handleInputChange = () => {
-    const selectedCurrency = document.querySelector("#currency-input").value;
-    const selectedDate = document.querySelector("#currency-date").value;
-    displayExchangeUI(selectedCurrency, selectedDate || null);
-  };
-
-  document.querySelector("#currency-input").addEventListener("input", handleInputChange);
-  document.querySelector("#currency-date").addEventListener("input", handleInputChange);
-}
-
 function initialize() {
   displayExchangeUI();
-  updateCurrency();
+  updateCurrencyUI($currencyCode, $currencyDate, $currencyList);
 }
 
 initialize();
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,7 @@
+const $currencyCode = document.querySelector("#currency-input");
+const $currencyDate = document.querySelector("#currency-date");
+const $currencyList = document.querySelector("#currency-list");
+
 function createExchangeTable(rates) {
   const $currencyTable = document.querySelector("#currency-rates");
   const $rates = Object.keys(rates);
@@ -34,4 +38,23 @@ function createCurrencyList(rates) {
     $li.appendChild($a);
     $currencyList.appendChild($li);
   });
+}
+
+function displayExchangeUI(currencyCode = null, date = null) {
+  return getExchangeRates(currencyCode, date)
+    .then((rates) => {
+      clearCurrenciesField();
+      createExchangeTable(rates);
+      createCurrencyList(rates);
+    })
+    .catch((error) => {
+      console.error("Error while attempting to display exchange rates", error);
+      throw error;
+    });
+}
+
+function updateCurrencyUI(currency, date, list) {
+  list.onclick = (event) => handleListChange(event, date);
+  currency.oninput = () => handleInputChange(currency, date);
+  date.oninput = handleInputDate(currency, date);
 }
