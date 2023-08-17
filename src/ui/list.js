c7fd8a0a0b9e7ac8ffe3705989964f7ddd3e0f29
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-08-17 17:28:57 -0600

    Update list module
    Now it creates a custome list for the table and the converter, this is being done in order to avoid a bug where the date is applying the changes on the converter dropdowns as well

--- src/ui/list.js
+++ src/ui/list.js
@@ -1,21 +1,39 @@
 import { getExistingCurrencies } from "../utils/general.js";
 
-export function createCurrencyList(currency) {
-  const $currencyLists = document.querySelectorAll(".currency-list");
+export function createTableCurrencyList(currency) {
+  const $currencyList = document.querySelector("#table-currency-list");
   const list = Object.keys(currency);
   const existingCurrencies = getExistingCurrencies(list);
 
-  $currencyLists.forEach(($currencyList) => {
-    $currencyList.innerText = "";
+  $currencyList.innerText = "";
+  list.forEach((currency, index) => {
+    const $list = document.createElement("li");
+    const $item = document.createElement("a");
+    $item.className = "dropdown-item text-start";
+    $item.href = "#";
+    $item.innerText = `${currency} - ${existingCurrencies[index]}`;
+
+    $list.appendChild($item);
+    $currencyList.appendChild($list);
+  });
+}
+
+export function createConverterCurrencyList(currency) {
+  const $currencyLists = document.querySelectorAll(".converter-currency-list");
+  const list = Object.keys(currency);
+  const existingCurrencies = getExistingCurrencies(list);
+
+  $currencyLists.forEach((currencyList) => {
     list.forEach((currency, index) => {
       const $list = document.createElement("li");
       const $item = document.createElement("a");
+
       $item.className = "dropdown-item text-start";
       $item.href = "#";
       $item.innerText = `${currency} - ${existingCurrencies[index]}`;
 
       $list.appendChild($item);
-      $currencyList.appendChild($list);
+      currencyList.appendChild($list);
     });
   });
 }
