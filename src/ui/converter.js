d3a737e9535f85bb1c93765e8e49031ebf29dfbf
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-08-19 20:05:30 -0600

    Creates converter module logic
    This is the logic of our converter, when the user selects two currenciens and an amout this logic will be in charge of passing it to the API and then enable all the controls over the results of it which will later work on.

--- src/ui/converter.js
+++ src/ui/converter.js
@@ -1,1 +1,66 @@
+import { getConvertionResults, getExchangeData } from "../api/exchange.js";
+import { handleListChange } from "./handlers.js";
+import { createConverterCurrencyList } from "./list.js";
 
+export function displayConvertionResults($from, $to, $amount) {
+  handleConvertButton();
+  getConvertionResults($from, $to, $amount).then((convertion) => {
+    const { from, to, query, date, result } = convertion;
+    console.log(convertion);
+    updateResults(from, to, query, date, result);
+  });
+}
+
+function updateResults(from, to, query, date, result) {
+  document.querySelector("#from-exchange").innerText = query.amount;
+  document.querySelector("#from-currency").innerText = ` ${from}`;
+  document.querySelector("#to-exchange").innerText = ` = ${result}`;
+  document.querySelector("#to-currency").innerText = ` ${to}`;
+  document.querySelector("#result-date").innerText = `${date} as date of Exchange`;
+}
+
+function enableConvertionResults() {
+  const $disabledResults = document.querySelectorAll(".disabled-text");
+  const $disabledButton = document.querySelector("#convert-time-button");
+
+  $disabledButton.classList.remove("disabled", "btn-secondary");
+  $disabledButton.classList.add("btn-primary");
+
+  $disabledResults.forEach((element) => {
+    element.classList.remove("text-secondary");
+    element.classList.add("text-primary");
+  });
+}
+
+function handleConvertButton() {
+  const $convertButton = document.querySelector("#convert-button");
+
+  $convertButton.onclick = () => {
+    const from = document.querySelector("#convert-from-input").value;
+    const to = document.querySelector("#convert-to-input").value;
+    const amount = document.querySelector("#convert-amount-input").value;
+    enableConvertionResults();
+    return displayConvertionResults(from, to, amount);
+  };
+}
+
+export function setFromListChanges(list) {
+  list.onclick = (currency) => {
+    handleListChange(currency, "#convert-from-input");
+  };
+}
+
+export function setToListChanges(list) {
+  list.onclick = (currency) => {
+    handleListChange(currency, "#convert-to-input");
+  };
+}
+
+export function setConverterCurrencyList() {
+  getExchangeData().then((exchange) => {
+    const { rates } = exchange;
+    createConverterCurrencyList(rates);
+  });
+}
+
+setConverterCurrencyList();
