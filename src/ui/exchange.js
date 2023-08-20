f60405eafc240433fa83b08c7cef5831b443e8b5
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-08-19 20:06:30 -0600

    Add updateExchangeConverter
    This will update everytime we click on our convert button, it also settles the currencies if they were selected through our dropdowns

--- src/ui/exchange.js
+++ src/ui/exchange.js
@@ -1,14 +1,22 @@
 import { displayExchangeTable, setTableListChanges, setTableCurrencyChanges, setTableDateChanges } from "./table.js";
+import { displayConvertionResults, setFromListChanges, setToListChanges } from "./converter.js";
 
 export function updateExchangeTable() {
   const $list = document.querySelector("#table-currency-list");
-  const $currency = document.querySelector("#currency-input");
+  const $currency = document.querySelector("#table-currency-input");
   const $date = document.querySelector("#currency-date");
 
-  displayExchangeTable();
   setTableListChanges($list, $date);
   setTableCurrencyChanges($currency, $date);
   setTableDateChanges($date, $currency);
+  displayExchangeTable();
 }
 
-export function updateExchangeConverter() {}
+export function updateExchangeConverter() {
+  const $fromList = document.querySelector("#convert-from-list");
+  const $toList = document.querySelector("#convert-to-list");
+
+  setFromListChanges($fromList);
+  setToListChanges($toList);
+  displayConvertionResults();
+}
