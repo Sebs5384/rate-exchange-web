fed88dcb968e9f486df60eafeff169bf64faaa58
Sebs5384 <operationmadjackal@hotmail.com>
2023-08-21 21:44:33 -0300

    Add validator function
    This will validate our fields and call the handler that will take care of errors.

--- src/ui/validation.js
+++ src/ui/validation.js
@@ -1,9 +1,13 @@
-function validateCurrencyFields(currency) {
-  if (/^$/.test(currency)) return "This currency field can't be empty";
-  if (!/^[^0-9]+$/.test(currency)) return "This field doesn't accept numbers";
-  if (!/^\S{3}$/.test(currency)) return "Invalid currency code";
+function validateCurrencyField(currencies) {
+  const currenciesErrors = {};
 
-  return "";
+  currencies.forEach((currency, index) => {
+    if (/^$/.test(currency)) currenciesErrors[index] = "This currency field can't be empty";
+    if (!/^[^0-9]+$/.test(currency)) currenciesErrors[index] = "This field doesn't accept numbers";
+    if (!/^\S{3}$/.test(currency)) currenciesErrors[index] = "Invalid currency code";
+  });
+
+  return currenciesErrors;
 }
 
 function validateAmountField(amount) {
@@ -12,4 +16,14 @@ function validateAmountField(amount) {
   if (!/^[^A-Z-a-z]+$/.test(amount)) return "This field doesn't accept letters";
 
   return "";
+}
+
+function validateCurrencyFields(currency) {
+  const validatedField = validateCurrencyField(currency);
+  const currencyError = {
+    "currency-field": validatedField,
+  };
+
+  const isSuccessful = handleErrorFields(currencyError) === 0;
+  return isSuccessful;
 }
