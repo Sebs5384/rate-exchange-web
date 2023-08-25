a8b2e31c1f2abd643b9e2bfa4f99e72f7f0dfac6
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-08-24 19:00:30 -0600

    Changes getCurrencyCode
    This new implementation allow the user the give a node list as parameter rather than just one element like before.

--- src/ui/utils.js
+++ src/ui/utils.js
@@ -1,6 +1,17 @@
-export function getCurrencyCode(currency) {
-  const currencyCode = currency.innerText.substring(0, 3);
-  return currencyCode;
+export function getCurrencyCode(currencies) {
+  const currenciesCode = [];
+
+  if (currencies instanceof NodeList) {
+    currencies.forEach((currency) => {
+      const currencyCode = currency.innerText.substring(0, 3);
+      currenciesCode.push(currencyCode);
+    });
+  } else {
+    const currencyCode = currencies.innerText.substring(0, 3);
+    currenciesCode.push(currencyCode);
+  }
+
+  return currenciesCode;
 }
 
 export function setElementVisibility(selector, visibility) {
