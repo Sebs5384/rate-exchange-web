3dae0a14dfa1df11fb10d3259912efd3c680b64a
Sebs5384 <operationmadjackal@hotmail.com>
2023-09-06 16:29:47 -0300

    Update table.cy
    This will now wait for the request made with "TEST" as base

--- cypress/e2e/table.cy.js
+++ cypress/e2e/table.cy.js
@@ -1,13 +1,16 @@
 describe("Exchange table testing", () => {
-  const URL = "http://127.0.0.1:8080";
   const currentDate = new Date().toISOString().split("T")[0];
+  const localhost = "http://127.0.0.1:8080";
+  const defaultExchangeURL = "https://api.exchangerate.host/latest?base=EUR&places=2";
+  const exchangeURL = "https://api.exchangerate.host/2020-01-01?base=ARS&places=2";
+  const testURL = `https://api.exchangerate.host/2023-09-04?base=TEST&places=2`;
 
   beforeEach(() => {
-    cy.visit(URL);
-    cy.intercept("GET", "https://api.exchangerate.host/latest?base=EUR&places=2", { fixture: "default-exchange" }).as("defaultExchange");
+    cy.visit(localhost);
+    cy.intercept("GET", defaultExchangeURL, { fixture: "default-exchange" }).as("defaultExchange");
   });
 
-  it("Should display the table on default correctly", () => {
+  it("Should display the table on default correctly upon initializing the page", () => {
     cy.wait("@defaultExchange").then(({ response }) => {
       const rates = Object.keys(response.body.rates);
       const base = response.body.base;
@@ -19,10 +22,11 @@ describe("Exchange table testing", () => {
   });
 
   it("Should display the exchange rates of the selected currency and date", () => {
-    cy.intercept("GET", "https://api.exchangerate.host/2020-01-01?base=ARS&places=2", { fixture: "exchange" }).as("exchange");
+    cy.intercept("GET", exchangeURL, { fixture: "exchange" }).as("exchange");
 
     cy.get("[data-cy='table-currency-list-button").click();
     cy.contains("ARS - Argentine Peso").click();
+
     cy.get("[data-cy='table-date-input']").type("2020-01-01");
 
     cy.wait("@exchange").then(({ response }) => {
@@ -39,13 +43,18 @@ describe("Exchange table testing", () => {
   });
 
   it("Should display Euro as default currency if no currency has match the typed one", () => {
+    cy.intercept("GET", testURL, { fixture: "default-exchange" }).as("testExchange");
+
     cy.get("[data-cy='table-code-input']").type("TEST");
-    cy.wait("@defaultExchange").then(({ response }) => {
+    cy.get("[data-cy='table-date-input']").type("2023-09-04");
+
+    cy.wait("@testExchange").then(({ response }) => {
       const rates = Object.keys(response.body.rates);
       const base = response.body.base;
+      const date = response.body.date;
 
       cy.get("[data-cy='current-currency-title']").should("have.text", `Currently displaying ${base}`);
-      cy.get("[data-cy='current-date-title']").should("have.text", `At ${currentDate} as date of exchange`);
+      cy.get("[data-cy='current-date-title']").should("have.text", `At ${date} as date of exchange`);
       cy.get("[data-cy='table-list-dropdown").find("li").should("have.length", rates.length);
       cy.get("[data-cy='table-body']").find("tr").should("have.length", rates.length);
     });
